07ded4e2d1124ffa6ebf21b60354720b9c0e232c
FishOrz <aa701a@gmail.com>
2021-05-09 21:09:38 +0800

    inverted symble

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ unsigned long lastUpTime = 0;
 unsigned long lastDownTime = 0;
 
 unsigned long intervalTime;
-bool state = 0; //1 = shutter down ; 0 = shutter up
+bool state = 0; //1 = bottom down ; 0 = bottom up
 void print_time(unsigned long time_millis);
 void trigger_The_Shutter();
 void setup()
@@ -46,15 +46,16 @@ void stepperMotorControl()
 void loop()
 {
   unsigned long currentTs = millis();
-  bool shouldUp = (lastUpTime == 0 || currentTs >= lastDownTime + idleDuration) && state == 1;
-  bool shouldDown = (currentTs >= lastUpTime + exposureDuration) && state == 0;
 
-  delay(1000);
-  Serial.print(currentTs);
+  bool shouldDown = (lastDownTime == 0 || currentTs >= lastUpTime + idleDuration) && state == 0;
+  bool shouldUp = (currentTs >= lastDownTime + exposureDuration) && state == 1;
+
+  delay(10);
+  // Serial.println(currentTs);
 
   if (shouldUp || shouldDown)
   {
-    Serial.print(shouldUp ? "UP" : "DOWN");
+    Serial.println(shouldUp ? "UP" : "DOWN");
     if (shouldUp)
     {
       lastUpTime = currentTs;
@@ -63,9 +64,10 @@ void loop()
     {
       lastDownTime = currentTs;
     }
-    Serial.print("Debug2");
+    // Serial.print("Debug2");
     trigger_The_Shutter();
     state = state == 1 ? 0 : 1;
+    Serial.println("");
   }
 }
 
