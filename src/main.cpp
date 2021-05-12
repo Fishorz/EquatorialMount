3c6e7f1baef61a3d3840b4e3ebc2863b93fca570
FishOrz <aa701a@gmail.com>
2021-05-12 08:41:31 +0800

    Calculate degree steps and s relation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ unsigned long idleDuration = 5000;
 unsigned long lastUpTime = 0;
 unsigned long lastDownTime = 0;
 int rotateSpeed = 1;
+int maxRotateSpeed = 20;
+int minRotateSpeed = 1;
 
 unsigned long intervalTime;
 bool state = 0; //1 = bottom down ; 0 = bottom up
@@ -15,6 +17,8 @@ void trigger_The_Shutter();
 void setup()
 {
   Serial.begin(9600);
+  pinMode(Step_Pin, OUTPUT);
+  pinMode(Dir_Pin, OUTPUT);
   Serial.println("start");
 }
 
@@ -41,11 +45,27 @@ void trigger_The_Shutter()
 }
 
 void stepperMotorControl()
+/* -----------------------
+1 rev = 360 degrees
+Use 1/N steps resolution
+1 full step = 1.8 degrees
+1 step = 1.8/N degrees
+For gear ratio 1:60
+1 step(pulse) = (1/N)/60 degrees = 1/(60N) degrees
+
+24hr 360degrees ;
+1hr 15 degrees;
+1mins = 0.25 degrees;
+1s = 0.25/60 degrees;
+1/1000s = 0.25/6000 degrees;
+1/1000s = (0.25/6000)/( 1/(60N) ) steps = 0.64 steps
+1.5625 (1/1000)s = 1 steps
+*/
 {
-    digitalWrite(Step_Pin, HIGH);
-    delayMicroseconds(10); //control the speed
-    digitalWrite(Step_Pin, LOW);
-    delayMicroseconds(10);
+  digitalWrite(Step_Pin, HIGH);
+  delayMicroseconds(10); //control the speed
+  digitalWrite(Step_Pin, LOW);
+  delayMicroseconds(10);
 }
 
 void print_time(unsigned long time_millis)
