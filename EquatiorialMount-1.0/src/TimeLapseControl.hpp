901fa3459c4b8f55982f90b443cc53aad3eaf879
skype <kinyan.chu@phrozen3d.com>
2022-02-09 14:54:39 +0800

    add logic about exposure or idle

--- EquatiorialMount-1.0/src/TimeLapseControl.hpp
+++ EquatiorialMount-1.0/src/TimeLapseControl.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "TakePhoto.hpp"
+#include "Logger.hpp"
 
 class timeLapseControl
 {
@@ -10,8 +11,10 @@ private:
     int _exposureTime;
     int _number;
     kickTrigger takePhoto;
+    Logger logger;
     bool isTriggerDone = false;
-
+    bool isExposuring = fasle;
+    bool waitingExposure = false;
     unsigned long _previousStartTakePhotoTime;
     unsigned long _previousEndTakePhotoTime;
 
@@ -20,6 +23,7 @@ public:
     setTimes(int intervalTime, int exposureTime);
     void setNumbers(int number);
     void runTimelapse();
+    void setpin() { takePhoto.setPin(32); }
 };
 
 void timeLapseControl::setTimes(int intervalTime, int exposureTime)
@@ -37,11 +41,33 @@ void timeLapseControl::runTimelapse()
 {
     unsigned long currentTimes = millis();
     unsigned long exposureTime = _exposureTime;
-    if (currentTimes - _previousStartTakePhotoTime > exposureTime)
+    insigned long intervalTime = _intervalTime;
+    if (currentTimes - _previousStartTakePhotoTime > exposureTime && isExposuring == true) // to control exposure
     {
         if (isTriggerDone == false)
         {
             isTriggerDone = takePhoto.trigger();
+            logger.print("gen signal to START Exposure");
+        }
+        else
+        {
+            isExposuring = false;
+            isTriggerDone = false;
+            waitingExposure = ture; //go to idle a intervalTimes
+        }
+    }
+    if (currentTimes - _previousEndTakePhotoTime > intervalTime && waitingExposure == ture) // to control idle status
+    {
+        if (isTriggerDone == false)
+        {
+            isTriggerDone = takePhoto.trigger();
+            logger.print("gen signal to END Exposure");
+        }
+        else
+        {
+            waitingExposure = false;
+            isTriggerDone = false;
+            isExposuring = ture; //next go to exposure
         }
     }
 }
--- EquatiorialMount-1.0/src/main.cpp
+++ EquatiorialMount-1.0/src/main.cpp
@@ -1,30 +1,12 @@
 #include <Arduino.h>
 #include "TimeLapseControl.hpp"
-kickTrigger Triggertest;
 
-bool istrigger = false;
-bool istriggedPrinted = false;
 void setup()
 {
-    Triggertest.setPin(32);
     Serial.begin(25000);
     delay(100);
 }
 
 void loop()
 {
-
-    if (istrigger == false)
-    {
-        istrigger = Triggertest.trigger();
-        Serial.println("trigging");
-    }
-    else
-    {
-        if (istriggedPrinted == false)
-        {
-            Serial.println("trigged");
-            istriggedPrinted = true;
-        }
-    }
 }
