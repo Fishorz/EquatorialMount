8346c068fac4795391e570007ddabf93cefa828a
skype <kinyan.chu@phrozen3d.com>
2022-02-25 15:02:01 +0800

    add full system discription

--- EquatiorialMount-1.0/src/main.cpp
+++ EquatiorialMount-1.0/src/main.cpp
@@ -1,5 +1,19 @@
+/*
+---------------------------------------------------------------------------------------
+----functions list----
+1.Manual local polar alignment( Use GPS and gyroscope{陀螺儀} ).
+2.input invterval time and exposure time take photo.
+3.follow polar alignment rotate.
+4.Input rotate speed to rotate.
+----User interface-----
+1.four button control.
+2.Onec TFT display.
+----additional functions----
+1.There is app to control.
+*/
 #include <Arduino.h>
 #include "TimeLapseControl.hpp"
+#include "kickTrigger.hpp"
 #include "meun.hpp"
 kickTrigger Triggertest;
 
