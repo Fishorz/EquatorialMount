4da5d2e335b8780ea1bdc62a20629fd829edf463
skype <kinyan.chu@phrozen3d.com>
2022-02-25 17:02:21 +0800

    fix format

--- EquatiorialMount-1.0/src/meun.hpp
+++ EquatiorialMount-1.0/src/meun.hpp
@@ -4,22 +4,20 @@
 
 /*meun description------------------------------------------------------------------
 
---mainMeun--              --sub meun--    
-interval Time Control     >>interval_mins 
+--mainMeun--              --sub meun--
+interval Time Control     >>interval_mins
                           >>interval_sec
                           >>interval_millisec
 exposure Time Control     >>exposure_mins
                           >>exposure_sec
                           >>exposure_millisec
-rotate Control            >>clockwise (CW)
-                          >>counterclockwise (CCW)
-mode Selection            >>Enable Rotate             //control rotation at taking photo
-                          >>Disable Rotate
+rotate Control            >>speed
+mode Selection            >>Follow mode                   //control rotation follow polar alignment at taking photo.
+                          >>Unfollow mode                 //control rotation follow user setup anytime.
 auto aim polar alignment  >>call auto aim polar function. //自動對準極軸
 
-
-about the buttom(s) 
-0{pervious meun} 
+about the buttom(s)
+0{pervious meun}
 1{i-- / meun-}  // time++ or meun++
 2{i++ / meun+}  //time-- or meun--
 3{select/start-stop take photo} 
@@ -69,8 +67,6 @@ private:
         exposure_sec,
         exposure_millisec,
     };
-
-   
 
 public:
     void getButtomStatus();
